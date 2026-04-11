e0533760804813b97077f69810337ed30390dccb
AverageDevver123 <justinharvey488@gmail.com>
2026-04-10 19:33:35 -0800

    feat(player): implement health mechanic (#2)

--- Player/player.gd
+++ Player/player.gd
@@ -19,10 +19,14 @@ var example : bool = true
 @onready var player_movement: PlayerMovement = $FiniteStateMachine/PlayerMovement
 @onready var player_default: PlayerDefault = $FiniteStateMachine/PlayerDefault
 
+@onready var healthBar = $HealthBar
+
 func _ready() -> void:
 	# FSM Signals
 	default_signals()
 	movement_signals()
+	
+	healthBar.set_health_bar(character_data.HP, maxHealth)
 
 func movement_signals() -> void:
 	player_movement.default.connect(fsm.change_state.bind(player_default))
@@ -43,3 +47,24 @@ func dir_flip(input_axis: float) -> void:
 		# These will be used later
 		detection.scale.x *= -1
 		player_collision.position.x *= -1
+
+
+var maxHealth = 3
+
+func take_damage(damage:int):
+	character_data.HP -= damage
+	if character_data.HP < 0: character_data.HP = 0
+	healthBar.change_health(- damage)
+
+func take_heal(heal:int):
+	character_data.HP += heal
+	healthBar.change_health(heal)
+
+func _on_damage_area_entered(area: Area2D) -> void:
+		if area.is_in_group("damage"):
+			take_damage(1)
+
+
+func _on_heal_area_entered(area: Area2D) -> void:
+	if area.is_in_group("heal"):
+		take_heal(1)
