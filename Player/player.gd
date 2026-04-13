01a33f9ee7fbe783d60ae2faea9951ff3d4b3ff5
Justin Harvey <justinharvey488@gmail.com>
2026-04-12 16:29:37 -0800

    Anchored Health Bar & added Death

--- Player/player.gd
+++ Player/player.gd
@@ -19,7 +19,7 @@ var example : bool = true
 @onready var player_movement: PlayerMovement = $FiniteStateMachine/PlayerMovement
 @onready var player_default: PlayerDefault = $FiniteStateMachine/PlayerDefault
 
-@onready var healthBar = $HealthBar
+@export var healthBar : ProgressBar
 
 func _ready() -> void:
 	# FSM Signals
@@ -55,6 +55,11 @@ func take_damage(damage:int):
 	character_data.HP -= damage
 	if character_data.HP < 0: character_data.HP = 0
 	healthBar.change_health(- damage)
+	if character_data.HP == 0:
+		die()
+		
+func die():
+	queue_free()
 
 func take_heal(heal:int):
 	character_data.HP += heal
--- health_bar.gd
+++ health_bar.gd
@@ -1,13 +1,16 @@
 extends ProgressBar
 
-@onready var label = $Label
+@export var label : Label
 
 func set_health_bar(health, maxHealth):
 	max_value = maxHealth
 	value = health
 	
-	label.text = str(health)
-	
+	if label:
+		label.text = str(health)
+
 func change_health(newValue):
 	value += newValue
-	label.text = str(value)
+	
+	if label:
+		label.text = str(value)
